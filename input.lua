0e147ab7d65cc35080f162a3a847dfa1281607a7
minufy <seomeenu@proton.me>
2026-06-29 01:33:08 +0900

    remove mb emu

--- input.lua
+++ input.lua
@@ -57,13 +57,3 @@ end
 Input.mb = {NewInput({1}), NewInput({2}), NewInput({3})}
 Input.wheel = NewInput()
 Input.wheel.up = false
-
-Input.mb_emu_1 = NewInput({"1"})
-Input.mb_emu_2 = NewInput({"3"})
-Input.mb_emu_3 = NewInput({"2"})
-
-Input.mb_emu = {
-    Input.mb_emu_1,
-    Input.mb_emu_2,
-    Input.mb_emu_3,
-}
